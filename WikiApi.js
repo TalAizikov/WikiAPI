ba59570fc0615e13d876f08f6efa527e19984bc3
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2018-01-10 09:37:28 -0500

    Working code + button

--- WikiApi.js
+++ WikiApi.js
@@ -1,24 +1,58 @@
 let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+
+let userInput;
+
+let counter = 0;
+
 
 
 function setup() {
-    noCanvas();
-    userInput = select('#userinput');
-    userInput.changed(goWiki);
-    goWiki();
-    
-    function goWiki() {
-        let term = userInput.value();
-        let url = searchUrl + term;
-        loadJSON(url, gotData, 'jsonp');
-    }
-    
-    function gotData {
-        let length = data[1].length;
-        let index = floor(random(len));
-        let title = data[1][index];
-        title = title.replace
-        console.log(data[1][index]);
-    }
-}
+  noCanvas();
+  userInput = select('#userinput');
+	document.getElementById('button').onclick = function() {
+		counter = 0;
+   startSearch(userInput);
+  //goWiki(userInput.value());
+};
+  
 
+  function startSearch() {
+    goWiki(userInput.value());
+  }
+
+  function goWiki(term) {
+    counter = counter + 1;
+
+    if (counter < 10) {
+      //let term = userInput.value();
+      let url = searchUrl + term;
+      loadJSON(url, gotSearch, 'jsonp');
+    }
+  }
+
+  function gotSearch(data) {
+    console.log(data);
+    let len = data[1].length;
+    let index = floor(random(len));
+    let title = data[1][index];
+    title = title.replace(/\s+/g, '_');
+    createDiv(title);
+    console.log('Querying: ' + title);
+    let url = contentUrl + title;
+    loadJSON(url, gotContent, 'jsonp');
+  }
+
+  function gotContent(data) {
+    let page = data.query.pages;
+    let pageId = Object.keys(data.query.pages)[0];
+    console.log(pageId);
+    let content = page[pageId].revisions[0]['*'];
+    console.log(content);
+    let wordRegex = /\b\w{4,}\b/g;
+    let words = content.match(wordRegex);
+    let word = random(words);
+    goWiki(word);
+    console.log(word);
+  }
+}
