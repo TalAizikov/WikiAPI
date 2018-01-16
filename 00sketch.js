80314e7b20c09fff154af425b73864c00c6f664e
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2018-01-15 21:03:52 -0500

    Create 00sketch.js

---
+++ 00sketch.js
@@ -0,0 +1,6 @@
+//let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+//let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+
+function setup() {
+    
+}
